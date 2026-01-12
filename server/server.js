6632bdd69caed65b5df7c7b461d5fc9f59d5c8c9
Harshini T <harshini2709005@gmail.com>
2026-01-12 14:39:10 +0530

    feat: add MongoDB connection setup in server.js

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
-
+import connectDB from "./config/mongodb.js" 
 const app = express();
 const port =process.env.PORT || 4000
+connectDB();
  app.use(express.json());
  app.use(cookieParser());
  app.use(cors({credentials:true}))
