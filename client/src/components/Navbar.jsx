19ccd29b4f060c82618d8013730fb5a5ec8d7ecd
Harshini T <harshini2709005@gmail.com>
2026-01-25 23:00:05 +0530

    feat: update header dropdown based on user verification and logout state

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,23 +2,35 @@ import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
+import { toast } from 'react-toastify'
+import axios from 'axios'
 
 const Navbar = () => {
   const navigate = useNavigate()
-
-  const {
+ const {
     userData,
     backendUrl,
     setUserData,
     setIsLoggedIn
   } = useContext(AppContext)
+  const logout = async () => {
+    try {
+      axios.defaults.withCredentials = true;
+      const { data } = await axios.post(backendUrl + "/api/auth/logout");
+      data.success && setIsLoggedIn(false);
+      data.success && setUserData(false);
+      navigate("/");
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
   return (
     <div className="w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0">
 
       <img
         src={assets.logo}
-        alt="logo"
+        alt=""
         className="w-28 sm:w-32"
       />
 
@@ -26,16 +38,16 @@ const Navbar = () => {
       {userData ? (
         <div className="relative group">
           <div className="w-8 h-8 flex justify-center items-center rounded-full bg-black text-white cursor-pointer">
-            {userData.name?.[0].toUpperCase()}
+            {userData.name[0].toUpperCase()}
           </div>
 
           {/* Dropdown */}
           <div className="absolute hidden group-hover:block right-0 z-10 text-black rounded pt-2">
             <ul className="list-none m-0 p-2 bg-gray-100 text-sm">
-              <li className="px-1 py-2 hover:bg-gray-200 cursor-pointer">
+              {!userData.isAccountVerified && <li className="px-1 py-2 hover:bg-gray-200 cursor-pointer">
                 Verify email
-              </li>
-              <li className="px-1 py-2 hover:bg-gray-100 cursor-pointer pr-10">
+              </li> }
+              <li onClick ={logout} className="px-1 py-2 hover:bg-gray-100 cursor-pointer pr-10">
                 Logout
               </li>
             </ul>
